58c24f9050980a1ea5cf49fca21b937fc9ae396d
Nickolazz1 <elanir358@gmail.com>
2019-12-25 15:45:58 +0200

    front-end tutorial

--- main/views.py
+++ main/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .models import Tutorial
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
 
 # Create your views here.
 def homepage(request):
@@ -10,16 +12,20 @@ def homepage(request):
                   template_name="main/home.html",
                   context={"tutorials": Tutorial.objects.all})
 
+
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New Account Created: {username}")
             login(request, user)
+            messages.info(request, f"You are now logged in as {username}")
             return redirect("main:homepage")
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}:{form.error_messages[msg]}")
 
 
     form = UserCreationForm
