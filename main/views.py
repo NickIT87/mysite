690c1e45843544d39687c37892e0c612c84ac436
Nickolazz1 <elanir358@gmail.com>
2019-12-26 17:19:45 +0200

    user login

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from .models import Tutorial
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from .forms import NewUserForm
 
 
 def homepage(request):
@@ -14,7 +15,7 @@ def homepage(request):
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = NewUserForm(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
@@ -26,7 +27,7 @@ def register(request):
             for msg in form.error_messages:
                 messages.error(request, f"{msg}:{form.error_messages[msg]}")
 
-    form = UserCreationForm
+    form = NewUserForm
     return render(request, "main/register.html", context={"form":form})
 
 
