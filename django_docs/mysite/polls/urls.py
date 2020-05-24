ccdc3cb2e912581247afb060401d944812881bdc
Nickolazz1 <elanir358@gmail.com>
2020-05-24 21:46:14 +0300

    less 3 compleete

--- django_docs/mysite/polls/urls.py
+++ django_docs/mysite/polls/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5/
+    # the 'name' value as called by the {% url %} template tag
     path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
