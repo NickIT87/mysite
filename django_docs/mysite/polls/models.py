f352dadd3bc0debbcffe821c53b9b25ed33fb9a1
Nickolazz1 <elanir358@gmail.com>
2020-05-25 21:17:58 +0300

    less 5 tests incompleete, need to review this lesson in future

--- django_docs/mysite/polls/models.py
+++ django_docs/mysite/polls/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+
 # Create your models here.
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -11,7 +13,10 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -20,3 +25,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
--- django_docs/mysite/polls/views.py
+++ django_docs/mysite/polls/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 from django.http import Http404
 from django.template import loader
 from .models import Choice, Question
+from django.utils import timezone
 
 
 # Create your views here.
@@ -19,7 +20,15 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        #return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions 
+        (not including those set to be published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
 
 #def detail(request, question_id):
 #    question = get_object_or_404(Question, pk=question_id)
@@ -27,6 +36,13 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 #def results(request, question_id):
 #    question = get_object_or_404(Question, pk=question_id)
