d1c6d25b23bf2690053dbb99b5a7c0d7840a51df
Nickolazz1 <elanir358@gmail.com>
2020-07-17 15:58:34 +0300

    import error

--- CoreySchaferBlog/flaskblog/main.py
+++ CoreySchaferBlog/flaskblog/main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
+
+# no import ! need to check
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'e09920cde250dbd5e37781ed1326db5f'
 
 posts = [
     {
@@ -25,6 +30,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = RegistrationForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
