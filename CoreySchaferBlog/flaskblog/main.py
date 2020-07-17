a23bfedb1f7cfaaffcd41ff8c3c45c866fbf089e
Nickolazz1 <elanir358@gmail.com>
2020-07-17 13:55:04 +0300

    flask project first definitions 17.07.2020

--- CoreySchaferBlog/flaskblog/main.py
+++ CoreySchaferBlog/flaskblog/main.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 20, 2020'
+    }
+]
 
 @app.route("/")
 @app.route("/home/")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts, title='Home')
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html", title='About')
 
 
 if __name__ == '__main__':
