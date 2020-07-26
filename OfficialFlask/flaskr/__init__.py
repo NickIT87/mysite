cb0d46b34a6c18ca1018c19902f1d5d1cb2f9ef6
Nickolazz1 <elanir358@gmail.com>
2020-07-26 15:16:22 +0300

    flask documentation auth

--- OfficialFlask/flaskr/__init__.py
+++ OfficialFlask/flaskr/__init__.py
@@ -27,12 +27,16 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return 'Hello, Worlda!'
+        return 'Hello, World!'
 
     from . import db
     db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
