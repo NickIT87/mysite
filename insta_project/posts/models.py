e5779b85f00a70ad082b46249fd79f9054a272bc
Nickolazz1 <elanir358@gmail.com>
2020-09-22 10:38:08 +0300

    test upload images

---
+++ insta_project/posts/models.py
@@ -0,0 +1,10 @@
+#posts/models.py
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.TextField()
+    cover = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
---
+++ insta_project/posts/views.py
@@ -0,0 +1,19 @@
+# posts/views.py
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+
+from .forms import PostForm
+from .models import Post
+
+# Create your views here.
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class CreatePostView(CreateView): # новый
+    model = Post
+    form_class = PostForm
+    template_name = 'post.html'
+    success_url = reverse_lazy('home')
