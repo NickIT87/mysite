9a245a2d5b64403563c5b3af718e712656fadf55
salamanka <elanir358@gmail.com>
2021-04-21 17:41:14 +0300

    PyCoding, internet shop l3complete

--- myshop/shop/mainapp/models.py
+++ myshop/shop/mainapp/models.py
@@ -31,7 +31,8 @@ class Product(models.Model):
     slug = models.SlugField(unique=True)
     image = models.ImageField(verbose_name='Изображение')
     description = models.TextField(verbose_name='Описание', null=True)
-    price = models.DateTimeField(max_digits=9, decimal_places=2, verbose_name='Цена')
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
+
 
     def __str__(self):
         return self.title
@@ -39,7 +40,7 @@ class Product(models.Model):
 
 class CartProduct(models.Model):
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
-    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
     product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
     qty = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая Цена')
@@ -50,7 +51,7 @@ class CartProduct(models.Model):
 
 class Cart(models.Model):
     owner = models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE)
-    products = models.ManyToManyField(CartProduct, blank=True)
+    products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
     total_products = models.PositiveIntegerField(default=0)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая Цена')
 
