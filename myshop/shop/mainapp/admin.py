d07885c80329d1a3e072e2be441973cd986a8078
salamanka <elanir358@gmail.com>
2021-04-26 09:56:47 +0300

    PyCoding, internet shop l9complete

--- myshop/shop/mainapp/admin.py
+++ myshop/shop/mainapp/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 
+from PIL import Image
 from .models import *
 
 # Register your models here.
+class NotebookAdminForm(ModelForm):
+    MIN_RESOLUTION = (400, 400)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = 'Загружайте изображения с минимальным разрешением {}x{}'.format(
+            *self.MIN_RESOLUTION
+        )
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Разрешение изображения меньше минимального!')
+        return image
+
+
 class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
