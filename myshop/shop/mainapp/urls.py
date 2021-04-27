d7fa68007c5985d3b5b2b9d9ccfede8081af9112
salamanka <elanir358@gmail.com>
2021-04-27 19:56:56 +0300

    l12 complete

--- myshop/shop/mainapp/urls.py
+++ myshop/shop/mainapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import test_view
+from .views import test_view, ProductDetailView
 
 
 urlpatterns = [
-    path('', test_view, name='base')
+    path('', test_view, name='base'),
+    path('products/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name='product_detail')
 ]
--- myshop/shop/mainapp/views.py
+++ myshop/shop/mainapp/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
+from .models import Notebook, Smartphone
 
 # Create your views here.
 def test_view(request):
     return render(request, 'base.html', {})
+
+#some err
+class ProductDetailView(DetailView):
+
+    CT_MODEL_MODEL_CLASS = {
+        'notebook': Notebook,
+        'smartphone': Smartphone
+    }
+
+    def dispatch(self, request, *args, **kwargs):
+        self.model = self.CT_MODEL_MODEL_CLASS[kwargs['ct_model']]
+        self.queryset = self.model._base_manager.all()
+        return super().dispatch(request, *args, **kwargs)
+
+    context_object_name = 'product'
+    template_name = 'product_detail.html'
+    slug_url_kwarg = 'slug'
+
