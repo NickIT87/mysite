ada98754aa79def4162b7abfff0b3020757eee66
Nickolazz1 <elanir358@gmail.com>
2021-05-09 12:08:39 +0300

    course pyDjango l6 incomplete

--- sandbox/mysite/mysite/urls.py
+++ sandbox/mysite/mysite/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from news.views import index
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('news/', index),
 ]
