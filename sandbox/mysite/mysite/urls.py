6aa5f2f0d0e5f4f76a9a9be234accde912017374
Nickolazz1 <elanir358@gmail.com>
2021-05-10 18:38:18 +0300

    course pyDjango l7 complete

--- sandbox/mysite/mysite/urls.py
+++ sandbox/mysite/mysite/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from news.views import index
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('news/', include('news.urls')),
+    path('', include('news.urls')),
 ]
