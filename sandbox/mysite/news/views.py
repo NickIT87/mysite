38daf06b9438e79db1e0e00fea372a2bc89fa990
Nickolazz1 <elanir358@gmail.com>
2021-05-16 12:17:44 +0300

    l21 complete (Reverse address resolution)

--- sandbox/mysite/news/views.py
+++ sandbox/mysite/news/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import News, Category
 
@@ -17,3 +16,9 @@ def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
     category = Category.objects.get(pk=category_id)
     return render(request, 'news/category.html', {'news':news, 'category': category})
+
+
+def view_news(request, news_id: int):
+    #news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {'news_item': news_item})
