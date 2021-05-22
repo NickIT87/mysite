92938a33355d1e539eb42044d7e385666bbde8e9
Nickolazz1 <elanir358@gmail.com>
2021-05-22 11:37:58 +0300

    ORM (pt 6) l37 complete

--- sandbox/mysite/news/templatetags/news_tags.py
+++ sandbox/mysite/news/templatetags/news_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from news.models import Category
 
@@ -8,10 +9,12 @@ register = template.Library()
 
 @register.simple_tag(name='get_list_categories')
 def get_categories():
-    return Category.objects.all()
+    #return Category.objects.all()
+    return Category.objects.annotate(cnt=Count('news'))
 
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='hello', arg2='world'):
-    categories = Category.objects.all()
+    #categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
     return {"categories":categories, "arg1":arg1, "arg2":arg2}
