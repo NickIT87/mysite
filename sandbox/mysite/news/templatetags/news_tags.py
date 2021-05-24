f9c93a939b382475eadaa13e5a5c193359ed69f5
Nickolazz1 <elanir358@gmail.com>
2021-05-24 15:27:18 +0300

    ORM (pt 3) l34 complete (found the necessary solution)

--- sandbox/mysite/news/templatetags/news_tags.py
+++ sandbox/mysite/news/templatetags/news_tags.py
@@ -9,12 +9,12 @@ register = template.Library()
 
 @register.simple_tag(name='get_list_categories')
 def get_categories():
-    #return Category.objects.all()
+    #return Category.objects.all() # Category.objects.filter(news__is_published__contains=1).annotate(cnt=Count('news'))
     return Category.objects.annotate(cnt=Count('news'))
 
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='hello', arg2='world'):
-    #categories = Category.objects.all()
-    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    #categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
+    categories = Category.objects.filter(news__is_published=True).annotate(cnt=Count('news'))
     return {"categories":categories, "arg1":arg1, "arg2":arg2}
