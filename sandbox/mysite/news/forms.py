3ba3c9b65cda168dc5410c39efeb438570659804
Nickolazz1 <elanir358@gmail.com>
2021-05-29 10:08:08 +0300

    user registration l46 pt2 completed.

--- sandbox/mysite/news/forms.py
+++ sandbox/mysite/news/forms.py
@@ -1,8 +1,32 @@
 from django import forms
 import re
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 #from .models import Category
 from  .models import News
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=150,
+        help_text='Максимум 150 символов',
+        label='Имя пользователя',
+        widget=forms.TextInput(attrs={"class": "form-control"}))
+    password1 = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(
+        label='E-mail',
+        widget=forms.EmailInput(attrs={"class": "form-control"}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class NewsForm(forms.ModelForm):
--- sandbox/mysite/news/views.py
+++ sandbox/mysite/news/views.py
@@ -3,18 +3,17 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 from .models import News, Category
-from .forms import NewsForm
+from .forms import NewsForm, UserRegisterForm
 from .utils import MyMixin
 
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Вы успешно зарегистрировались')
@@ -22,7 +21,7 @@ def register(request):
         else:
             messages.error(request, 'Ошибка регистрации')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'news/register.html', {'form': form})
 
 
