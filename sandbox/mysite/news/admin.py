1e63abc7054cef1aa0368ea9682adf6e93dd3590
Nickolazz1 <elanir358@gmail.com>
2021-05-30 09:48:04 +0300

    ckeditor l49 completed.

--- sandbox/mysite/news/admin.py
+++ sandbox/mysite/news/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 from .models import News, Category
 
 # Register your models here.
+class NewsAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = News
+        fields = '__all__'
+
+
 class NewsAdmin(admin.ModelAdmin):
+    form = NewsAdminForm
     list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
