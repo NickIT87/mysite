5b6a680f69085d880cb3c2884a69d54ad8565bc1
Nickolazz1 <elanir358@gmail.com>
2021-05-30 14:42:46 +0300

    cache l51 completed.

--- sandbox/mysite/news/templatetags/news_tags.py
+++ sandbox/mysite/news/templatetags/news_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.db.models import Count, F
+from django.core.cache import cache
 
 from news.models import Category
 
@@ -18,4 +19,8 @@ def show_categories(arg1='hello', arg2='world'):
     #categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
     #categories = Category.objects.filter(news__is_published=True).annotate(cnt=Count('news'))
     categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
+    # categories = cache.get('categories')
+    # if not categories:
+    #     categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
+    #     cache.set('categories', categories, 30)
     return {"categories":categories, "arg1":arg1, "arg2":arg2}
--- sandbox/mysite/news/urls.py
+++ sandbox/mysite/news/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from .views import *
 
@@ -10,6 +11,7 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('test/', test, name='test'),
     #path('', index, name='home'),
+    #path('', cache_page(60)(HomeNews.as_view()), name='home'),
     path('', HomeNews.as_view(), name='home'),
     #path('cat/<int:category_id>', get_category, name='category'),
     path('cat/<int:category_id>', NewsByCategory.as_view(
