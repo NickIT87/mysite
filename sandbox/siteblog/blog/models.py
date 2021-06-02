29b8ec36f9576dda70c26c89e9a253c1d3671fe6
Nickolazz1 <elanir358@gmail.com>
2021-06-02 11:18:36 +0300

    part2 l7 (gl58) completed.

--- sandbox/siteblog/blog/models.py
+++ sandbox/siteblog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 '''
@@ -22,6 +23,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={"slug": self.slug})
 
     class Meta:
         verbose_name = 'Категория(ю)'
