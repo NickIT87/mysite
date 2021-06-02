0a024433073daa576bdb5ff6a50288be7f7d5f6a
Nickolazz1 <elanir358@gmail.com>
2021-06-02 11:28:47 +0300

    part2 l8 (gl59) completed.

--- sandbox/siteblog/blog/urls.py
+++ sandbox/siteblog/blog/urls.py
@@ -5,5 +5,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('category/<str:slug>', get_category, name='category'),
+    path('category/<str:slug>/', get_category, name='category'),
 ]
