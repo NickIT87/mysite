372271f9d4d89de3c2a02bbd5222a5223b430a59
Nickolazz1 <elanir358@gmail.com>
2021-06-02 12:01:08 +0300

    part2 l9 (gl60) completed.

--- sandbox/siteblog/blog/urls.py
+++ sandbox/siteblog/blog/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', Home.as_view(), name='home'),
     path('category/<str:slug>/', get_category, name='category'),
+    path('post/<str:slug>/', get_post, name='post'),
 ]
--- sandbox/siteblog/blog/views.py
+++ sandbox/siteblog/blog/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Post, Category, Tag
+
+
+class Home(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 8
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Classic Blog Design'
+        return context
 
 
 def index(request):
     return render(request, 'blog/index.html')
 
+
 def get_category(request, slug):
+    return render(request, 'blog/category.html')
+
+
+def get_post(request, slug):
     return render(request, 'blog/category.html')
