a940af57168e2e37ef92f29bde6fcb5e77747fc5
Nickolazz1 <elanir358@gmail.com>
2021-06-03 10:05:39 +0300

    part2 l12 (gl63) completed.

--- sandbox/siteblog/blog/urls.py
+++ sandbox/siteblog/blog/urls.py
@@ -6,5 +6,6 @@ from .views import *
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('category/<str:slug>/', PostsByCategory.as_view(), name='category'),
-    path('post/<str:slug>/', get_post, name='post'),
+    path('tag/<str:slug>/', PostsByTag.as_view(), name='tag'),
+    path('post/<str:slug>/', GetPost.as_view(), name='post'),
 ]
--- sandbox/siteblog/blog/views.py
+++ sandbox/siteblog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.db.models import F
 
 from .models import Post, Category, Tag
 
@@ -31,13 +32,18 @@ class PostsByCategory(ListView):
         return context
 
 
-def index(request):
-    return render(request, 'blog/index.html')
+class PostsByTag(ListView):
+    pass
 
 
-def get_category(request, slug):
-    return render(request, 'blog/category.html')
+class GetPost(DetailView):
+    model = Post
+    template_name = 'blog/single.html'
+    context_object_name = 'post'
 
-
-def get_post(request, slug):
-    return render(request, 'blog/category.html')
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
