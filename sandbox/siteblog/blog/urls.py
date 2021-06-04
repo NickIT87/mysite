11a71de2470f96027984f6b9ac02170060193982
Nickolazz1 <elanir358@gmail.com>
2021-06-04 15:17:15 +0300

    part2 l16 (gl67) completed.

--- sandbox/siteblog/blog/urls.py
+++ sandbox/siteblog/blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('category/<str:slug>/', PostsByCategory.as_view(), name='category'),
     path('tag/<str:slug>/', PostsByTag.as_view(), name='tag'),
     path('post/<str:slug>/', GetPost.as_view(), name='post'),
+    path('search/', Search.as_view(), name='search'),
 ]
