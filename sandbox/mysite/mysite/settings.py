c2367b6451e9282ef4dcd4082722ea95a465ea6a
Nickolazz1 <elanir358@gmail.com>
2021-06-04 18:49:39 +0300

    part2 l19 (gl70) completed.

--- sandbox/mysite/mysite/settings.py
+++ sandbox/mysite/mysite/settings.py
@@ -224,3 +224,5 @@ CACHES = {
         'LOCATION': os.path.join(BASE_DIR, 'django_cache'),
     }
 }
+
+MPTT_ADMIN_LEVEL_INDENT = 20
--- sandbox/mysite/testapp/admin.py
+++ sandbox/mysite/testapp/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from .models import Rubric
+# from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
+
+from .models import Rubric, Article
 
 
-admin.site.register(Rubric)
+# admin.site.register(Rubric, MPTTModelAdmin)
+admin.site.register(
+    Rubric,
+    DraggableMPTTAdmin,
+    list_display=(
+        'tree_actions',
+        'indented_title',
+    ),
+    list_display_links=(
+        'indented_title',
+    ),
+)
+admin.site.register(Article)
