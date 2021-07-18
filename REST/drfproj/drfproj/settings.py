bcd77fd74b10a8dcd24f29ac5b148e2a6206ca96
Nickolazz1 <elanir358@gmail.com>
2021-07-18 12:16:58 +0300

    tokens ...

--- REST/drfproj/drfproj/settings.py
+++ REST/drfproj/drfproj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'drfapp'
 ]
 
@@ -125,3 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': ( 'rest_framework.authentication.TokenAuthentication', ),
+}
--- REST/drfproj/drfproj/views.py
+++ REST/drfproj/drfproj/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from drfapp.serializers import StudentSerializer
 from drfapp.models import Student
 
 
 class TesView(APIView):
+
+    permission_classes = (IsAuthenticated)
+
     def get(self, request, *args, **kwargs):
-        data = {
-            'username': 'admin',
-            'no_of_years_active': 10
-        }
-        return Response(data)
+        # data = {
+        #     'username': 'admin',
+        #     'no_of_years_active': 10
+        # }
+        qs = Student.objects.all()
+        student1 = qs.first()
+        #serializer = StudentSerializer(qs, many=True)
+        serializer = StudentSerializer(student1)
+        return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
         serializer = StudentSerializer(data=request.data)
