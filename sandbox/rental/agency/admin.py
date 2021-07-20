bea74dd013761726da04277b098b401be38b352f
Nickolazz1 <elanir358@gmail.com>
2021-07-20 19:56:30 +0300

    image upload

--- sandbox/rental/agency/admin.py
+++ sandbox/rental/agency/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class GalleryInline(admin.TabularInline):
+    fk_name = 'product'
+    model = Gallery
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [GalleryInline,]
+
+
+admin.site.register(Gallery)
--- sandbox/rental/agency/models.py
+++ sandbox/rental/agency/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.TextField()
+
+
+class Gallery(models.Model):
+    image = models.ImageField(upload_to='gallery')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+
+    def delete(self):
+        self.image.delete()
+        super().delete()
