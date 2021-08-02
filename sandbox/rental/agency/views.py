75a8ee3d7f4129d2e4c5c3ad2ab68228437bafcc
Nickolazz1 <elanir358@gmail.com>
2021-08-02 13:43:02 +0300

    templates settings

--- sandbox/rental/agency/views.py
+++ sandbox/rental/agency/views.py
@@ -3,4 +3,9 @@ from django.views.generic.base import TemplateView
 
 # Create your views here.
 class HomePageView(TemplateView):
-    template_name = "index.html"
+    template_name = "agency/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Home"
+        return context
