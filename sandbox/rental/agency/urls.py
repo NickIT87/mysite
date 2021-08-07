ee9dc4cc73f0301445cc1a101517d5c49118de8b
Nickolazz1 <elanir358@gmail.com>
2021-08-07 20:11:40 +0300

    apartments sidebar testing

--- sandbox/rental/agency/urls.py
+++ sandbox/rental/agency/urls.py
@@ -6,4 +6,6 @@ from .views import *
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('apartments/', ApartmentsView.as_view(), name='apartments'),
+    path('apartments/sale/', ApartmentsAllSaleView.as_view(), name='apartmentsAllSale'),
+    path('apartments/rental/', ApartmentsAllRentalView.as_view(), name='apartmentsAllRental'),
 ]
--- sandbox/rental/agency/views.py
+++ sandbox/rental/agency/views.py
@@ -27,3 +27,31 @@ class ApartmentsView(ListView):
 
     def get_queryset(self):
         return Apartment.objects.all()
+
+
+class ApartmentsAllSaleView(ListView):
+    model = Apartment
+    template_name = "agency/apartments.html"
+    context_object_name = 'flats'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Квартиры продажа"
+        return context
+
+    def get_queryset(self):
+        return Apartment.objects.filter(proposal_type='Продажа')
+
+
+class ApartmentsAllRentalView(ListView):
+    model = Apartment
+    template_name = "agency/apartments.html"
+    context_object_name = 'flats'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Квартиры аренда"
+        return context
+
+    def get_queryset(self):
+        return Apartment.objects.filter(proposal_type='Аренда')
