9a41186261dcb9734c53979ec54411863be324c8
NickIT87 <elanir358@gmail.com>
2023-11-12 18:11:49 +0200

    start from images

--- cms_tut/wagtailcms/mysite/blog/models.py
+++ cms_tut/wagtailcms/mysite/blog/models.py
@@ -13,6 +13,13 @@ class BlogIndexPage(Page):
         FieldPanel('intro')
     ]
 
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
 
 class BlogPage(Page):
     date = models.DateField("Post date")
